6375545913a2c9f1b639100a787f0b9f60743712
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-18 20:17:35 +0100

    Modified App to import auth component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { Auth } from './components/auth';
 
 function App() {
 	return (
 		<div className="App">
-		
+			<Auth />
+
 		</div>
 	);
 }
+
+
+// firebase login
+// firebase init
+//firebase deploy
 
 export default App;
